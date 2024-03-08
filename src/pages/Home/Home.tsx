1328d1dc6b3c56815328700fb48ea322ddbf9cf9
BrunoPerez <brunoperez24793@gmail.com>
2024-03-08 16:15:46 -0300

    adding header

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Header from "../../components/Header";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+    </View>
+  )
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  }
+})
